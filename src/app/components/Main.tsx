e589b7c218d43ab418925c3b72d94070cd26d587
b6540304091 <phiromrat.l@ku.th>
2024-07-17 15:10:05 +0700

    first commit

---
+++ src/app/components/Main.tsx
@@ -0,0 +1,188 @@
+import React from "react";
+
+export default function Main() {
+  return (
+    <div>
+      <div className="flex justify-center ">
+        <div className="flex flex-col justify-center items-center bg-slate-100 max-w-7xl">
+          <div className="text-3xl text-green-600 font-bold mt-4 ">
+            การวางแผนและการบริหารนโยบายในยุคดิจิทัล
+          </div>
+          <div className="text-2xl text-black font-bold mt-2 ">
+            What our Customers Say
+          </div>
+          <div className="flex flex-col items-center justify-evenly my-12 lg:px-10 lg:flex-row space-y-8 lg:space-y-0 ">
+            <div className="flex flex-col justify-center items-center p-10 bg-white shadow w-[90%] md:w-[70%] lg:w-[30%] overflow-hidden">
+              <div className="  "></div>
+              <div className="flex flex-col -mt-20 -mr-[352px] hidden xl:block">
+                <div className="w-2 h-14 bg-blue-900"></div>
+                <div className="w-2 h-14 bg-green-500"></div>
+              </div>
+              <h1 className="text-xl tracking-tight font-semibold text-green-600 uppercase   ">
+                1. Best Components
+              </h1>
+              <p className="text-black mt-3 text-center w-[90%] lg:w-[100%]">
+                {" "}
+                Tailblocks provides best Tailwind CSS Components and Blocks to
+                create an unique websites within minutes. It has upto 60+ free
+                components for front-end Web Development.
+              </p>
+              <img
+                src="https://source.unsplash.com/150x150/?man"
+                className="rounded-full mt-4"
+                alt=""
+              />
+              <div className="flex flex-col -ml-[352px] mt-72 absolute hidden xl:block">
+                <div className="w-2 h-14 bg-blue-900"></div>
+                <div className="w-2 h-14 bg-green-500"></div>
+              </div>
+              <h1 className="text-lg font-semibold text-green-600 uppercase mt-1">
+                mark warner
+              </h1>
+              <h1 className="text-lg font-semibold text-black uppercase -mt-1">
+                ceo / director
+              </h1>
+            </div>
+            <div className="flex flex-col justify-center items-center p-10 bg-white shadow w-[90%] md:w-[70%] lg:w-[30%] overflow-hidden">
+              <div className="  "></div>
+              <div className="flex flex-col -mt-20 -mr-[352px] hidden xl:block">
+                <div className="w-2 h-14 bg-blue-900"></div>
+                <div className="w-2 h-14 bg-green-500"></div>
+              </div>
+              <h1 className="text-xl tracking-tight font-semibold text-green-600 uppercase   ">
+                2. Amazing Designs
+              </h1>
+              <p className="text-black mt-3 text-center w-[90%] lg:w-[100%]">
+                {" "}
+                Tailblocks provides best Tailwind CSS Components and Blocks to
+                create an unique websites within minutes. It has upto 60+ free
+                components for front-end Web Development.
+              </p>
+              <img
+                src="https://source.unsplash.com/150x150/?girl"
+                className="rounded-full mt-4"
+                alt=""
+              />
+              <div className="flex flex-col -ml-[352px] mt-72 absolute hidden xl:block">
+                <div className="w-2 h-14 bg-blue-900"></div>
+                <div className="w-2 h-14 bg-green-500"></div>
+              </div>
+              <h1 className="text-lg font-semibold text-green-600 uppercase mt-1">
+                alexa daniel
+              </h1>
+              <h1 className="text-lg font-semibold text-black uppercase -mt-1">
+                ceo / director
+              </h1>
+            </div>
+            <div className="flex flex-col justify-center items-center p-10 bg-white shadow w-[90%]  md:w-[70%] lg:w-[30%] overflow-hidden">
+              <div className="  "></div>
+              <div className="flex flex-col -mt-20 -mr-[352px] hidden xl:block">
+                <div className="w-2 h-14 bg-blue-900"></div>
+                <div className="w-2 h-14 bg-green-500"></div>
+              </div>
+              <h1 className="text-xl tracking-tight font-semibold text-green-600 uppercase   ">
+                3. Impressive
+              </h1>
+              <p className="text-black mt-3 text-center w-[90%] lg:w-[100%]">
+                {" "}
+                Tailblocks provides best Tailwind CSS Components and Blocks to
+                create an unique websites within minutes. It has upto 60+ free
+                components for front-end Web Development.
+              </p>
+              <img
+                src="https://source.unsplash.com/150x150/?woman"
+                className="rounded-full mt-4"
+                alt=""
+              />
+              <div className="flex flex-col -ml-[352px] mt-72 absolute hidden xl:block">
+                <div className="w-2 h-14 bg-blue-900"></div>
+                <div className="w-2 h-14 bg-green-500"></div>
+              </div>
+              <h1 className="text-lg font-semibold text-green-600 uppercase mt-1">
+                jessy bush
+              </h1>
+              <h1 className="text-lg font-semibold text-black uppercase -mt-1">
+                ceo / director
+              </h1>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div classNameName="flex justify-center bg-blue-500 p-4">
+        <div classNameName="flex flex-col justify-center items-center ">
+          <div classNameName="text-white text-3xl font-medium">
+            Our Trusted Clients
+          </div>
+          <div classNameName="flex flex-col md:flex-row max-w-7xl justify-center items-center ">
+            <div classNameName="overflow-hidden w-full m-4 flex justify-center bg-white rounded-lg  md:w-[33%] px-8">
+              <div classNameName="flex flex-col md:flex-row items-center justify-center  ">
+                <div classNameName="  items-center justify-center flex py-2">
+                  <div classNameName="flex flex-col  items-center justify-center text-center">
+                    <img
+                      src="https://source.unsplash.com/150x150/?man,boy"
+                      alt=""
+                      classNameName="rounded-full"
+                    />
+                    <div classNameName="text-stone-600 font-medium m-2">
+                      {" "}
+                      Tailblocks provides best Tailwind CSS Components and
+                      Blocks to create an unique websites within minutes.
+                    </div>
+                    <div classNameName="font-bold">John Doe</div>
+                    <div classNameName="text-cyan-600 italic">
+                      <a href="#">Board Director of Tailblocks</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div classNameName="overflow-hidden w-full m-4 flex justify-center bg-white rounded-lg  md:w-[33%] px-8">
+              <div classNameName="flex flex-col md:flex-row items-center justify-center  ">
+                <div classNameName="  items-center justify-center flex py-2">
+                  <div classNameName="flex flex-col  items-center justify-center text-center">
+                    <img
+                      src="https://source.unsplash.com/150x150/?girl"
+                      alt=""
+                      classNameName="rounded-full"
+                    />
+                    <div classNameName="text-stone-600 font-medium m-2">
+                      {" "}
+                      Tailblocks provides best Tailwind CSS Components and
+                      Blocks to create an unique websites within minutes.
+                    </div>
+                    <div classNameName="font-bold">Jessie</div>
+                    <div classNameName="text-cyan-600 italic">
+                      <a href="#">MD of Tailblocks</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div classNameName="overflow-hidden w-full m-4 flex justify-center bg-white rounded-lg  md:w-[33%] px-8">
+              <div classNameName="flex flex-col md:flex-row items-center justify-center  ">
+                <div classNameName="  items-center justify-center flex py-2">
+                  <div classNameName="flex flex-col  items-center justify-center text-center">
+                    <img
+                      src="https://source.unsplash.com/150x150/?girl,woman"
+                      alt=""
+                      classNameName="rounded-full"
+                    />
+                    <div classNameName="text-stone-600 font-medium m-2">
+                      {" "}
+                      Tailblocks provides best Tailwind CSS Components and
+                      Blocks to create an unique websites within minutes.
+                    </div>
+                    <div classNameName="font-bold">Davina Claire</div>
+                    <div classNameName="text-cyan-600 italic">
+                      <a href="#">Board Director of Tailblocks</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
